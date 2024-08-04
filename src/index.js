91a9fc6c3b600453524b13bbb29e25161fa5143f
Valentin-Sosa <valentin.sosa093@gmail.com>
2024-08-04 00:18:01 -0300

    style of the page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './style.css';
-import {format, getDay, toDate, fromUnixTime} from 'date-fns';
+import {format, fromUnixTime} from 'date-fns';
 
 const locationInput = document.getElementById("location");
 const form = document.querySelector("form");
+const metricBtn = document.getElementById("metric");
+const usBtn = document.getElementById("us");
+
 let unit = "metric";
 let unitSymbol = "ºC";
 let windUnit = "Km/h";
-
+let location = "";
 
 async function getData(location)
 {
@@ -18,12 +21,36 @@ async function getData(location)
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    getData(locationInput.value);
+    location = locationInput.value;
+    getData(location);
     form.reset();
 });
 
+metricBtn.addEventListener("click", ()=>
+{
+    unit = "metric";
+    unitSymbol = "ºC";
+    windUnit = "Km/h";
+    metricBtn.className = "active";
+    usBtn.className = "";
+    if(location !== "") getData(location);
+});
+
+usBtn.addEventListener("click", ()=>
+{
+    unit = "us";
+    unitSymbol = "ºF";
+    windUnit = "mph"
+    metricBtn.className = "";
+    usBtn.className = "active";
+    if(location !== "") getData(location);
+    
+})
+
 function showData(weatherData)
 {
+    const dataSection = document.querySelector(".data-section");
+
     const locationName = document.querySelector(".location-name");
     const locationDate = document.querySelector(".date");
 
@@ -37,34 +64,33 @@ function showData(weatherData)
     const sunrise = document.getElementById("sunrise");
     const sunset = document.getElementById("sunset");
     const wind = document.getElementById("wind");
-    const cloudCover = document.getElementById("cloud-cover");
     const precipprob = document.getElementById("precipprob");
 
     const weekDays = document.querySelectorAll(".day");
 
+    dataSection.className = "data-section active";
 
-    console.log(weatherData);
     locationName.textContent = weatherData.resolvedAddress;
     locationDate.textContent = format(new Date(), "eeee d MMMM yyyy | HH:mm");
 
     temp.textContent = `${weatherData.currentConditions.temp}${unitSymbol}`;
     condition.textContent = weatherData.currentConditions.conditions;
     feelsLike.textContent = `Feels like ${weatherData.currentConditions.feelslike}${unitSymbol}`;
-    tempRange.textContent = `Temp max: ${weatherData.days[0].tempmax}${unitSymbol} Temp min: ${weatherData.days[0].tempmin}${unitSymbol}`;
+    tempRange.textContent = `${weatherData.days[0].tempmax}${unitSymbol} / ${weatherData.days[0].tempmin}${unitSymbol}`;
     
     humidity.textContent = `${weatherData.currentConditions.humidity}%`;
     uvIndex.textContent = weatherData.currentConditions.uvindex;
     sunrise.textContent = format(fromUnixTime(weatherData.currentConditions.sunriseEpoch), "HH:mm");
     sunset.textContent = format(fromUnixTime(weatherData.currentConditions.sunsetEpoch), "HH:mm");
-    wind.textContent = `${weatherData.currentConditions.windseepd} ${windUnit}`;
-    cloudCover.textContent = `${weatherData.currentConditions.cloudcover}%`;
+    wind.textContent = `${weatherData.currentConditions.windspeed} ${windUnit}`;
     precipprob.textContent = `${weatherData.currentConditions.precipprob}%`;
 
     weekDays.forEach(day =>
     {
         const index = day.dataset.index;
         const weekDay = format(fromUnixTime(weatherData.days[index].datetimeEpoch), "EEEE");
-        day.textContent = `${weekDay} ${weatherData.days[index].tempmax}${unitSymbol} ${weatherData.days[index].tempmin}${unitSymbol}`;
+        //day.textContent = `${weekDay} ${weatherData.days[index].tempmax}${unitSymbol} | ${weatherData.days[index].tempmin}${unitSymbol}`;
+        day.innerHTML = `<span class="wday">${weekDay}</span> ${weatherData.days[index].tempmax}${unitSymbol} | ${weatherData.days[index].tempmin}${unitSymbol}`;
     }
     )
 }
